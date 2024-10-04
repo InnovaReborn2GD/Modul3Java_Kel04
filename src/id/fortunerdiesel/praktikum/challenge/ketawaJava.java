62e66dec95e970b34bbca061b4ec48351a7a83c3
Athari Muhammad <70275655+InnovaReborn2GD@users.noreply.github.com>
2024-10-05 05:36:30 +0700

    nambah ketawa java

---
+++ src/id/fortunerdiesel/praktikum/challenge/ketawaJava.java
@@ -0,0 +1,17 @@
+package id.fortunerdiesel.praktikum.challenge;
+
+public class ketawaJava {
+    public static void main(String[] args){
+        String ketawa = "HA";
+        int jmlKetawa = 5;
+
+        System.out.println("ini dia ketawa java horizontal");
+        for (int i = 0; i <= jmlKetawa; i++){
+            System.out.print(ketawa);
+        }
+        System.out.println("\n\nini dia ketawa java vertikal");
+        for (int i = 1 ; i <= 4; i++){
+            System.out.println(ketawa);
+        }
+    }
+}
