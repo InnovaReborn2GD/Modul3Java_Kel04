b7f13f9545b167050c8178db833b26ae69ebf019
Athari Muhammad <70275655+InnovaReborn2GD@users.noreply.github.com>
2024-10-05 12:57:53 +0700

    menambahkan user input untuk jenis ketawa dan jumlah perulangan

--- src/id/fortunerdiesel/praktikum/challenge/ketawaJava.java
+++ src/id/fortunerdiesel/praktikum/challenge/ketawaJava.java
@@ -1,14 +1,21 @@
 package id.fortunerdiesel.praktikum.challenge;
+import java.util.Scanner;
 
 public class ketawaJava {
     public static void main(String[] args){
-        String ketawa = "HA";
-        int jmlKetawa = 5;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ketawamu mau kek mana? ");
+        String ketawa = input.nextLine();
+
+        System.out.println("Mau berapa banyak (diulangnya)? ");
+        int jmlKetawa = input.nextInt();
 
         System.out.println("ini dia ketawa java horizontal");
         for (int i = 0; i <= jmlKetawa; i++){
             System.out.print(ketawa);
         }
+
         System.out.println("\n\nini dia ketawa java vertikal");
         for (int i = 1 ; i <= 4; i++){
             System.out.println(ketawa);
